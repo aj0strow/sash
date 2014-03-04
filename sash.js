8f0570f5e926e9dfac330cd4c28be39b988ecafc
AJ Ostrow <alexander.ostrow@gmail.com>
2014-03-03 23:02:41 -0500

    sash.prop

--- sash.js
+++ sash.js
@@ -2,6 +2,13 @@ module.exports = {
 
   identity: function (x) {
     return x;
+  },
+
+  prop: function (key) {
+    return function (object) {
+      if (object === null || object === undefined) return undefined;
+      return object[key];
+    };
   }
 
 };
--- test/sash.test.js
+++ test/sash.test.js
@@ -1,9 +1,24 @@
 describe('Sash', function () {
 
   describe('.identity', function () {
+    var object = {};
+
     it('should return whats passed in', function () {
-      var object = {};
       assert.equal(object, sash.identity(object));
+    });
+  });
+
+  describe('.property', function () {
+    var object = { a: 1 };
+    var fn = sash.prop('a');
+
+    it('should return undefined if object null', function () {
+      assert.equal(undefined, fn(null));
+      assert.equal(undefined, fn(undefined));
+    });
+
+    it('should return the property', function () {
+      assert.equal(1, fn(object));
     });
   });
 
