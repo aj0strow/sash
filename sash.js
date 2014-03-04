589780beb5db497eb548ca5fc9d2633afbf2efaf
AJ Ostrow <alexander.ostrow@gmail.com>
2014-03-04 00:27:05 -0500

    prop takes a path of keys now

--- sash.js
+++ sash.js
@@ -1,13 +1,18 @@
+var slice = Array.prototype.slice;
+
 module.exports = {
 
   identity: function (x) {
     return x;
   },
 
-  prop: function (key) {
+  prop: function () {
+    var keys = slice.call(arguments);
     return function (object) {
-      if (object === null || object === undefined) return undefined;
-      return object[key];
+      return keys.reduce(function (object, key) {
+        if (object === null || object === undefined) return undefined;
+        return object[key];
+      }, object);
     };
   }
 
--- test/sash.test.js
+++ test/sash.test.js
@@ -8,17 +8,28 @@ describe('Sash', function () {
     });
   });
 
-  describe('.property', function () {
-    var object = { a: 1 };
-    var fn = sash.prop('a');
+  describe('.prop', function () {
+    var object = { a: 1, b: { c: [ 2 ] } };
 
     it('should return undefined if object null', function () {
+      var fn = sash.prop('x');
       assert.equal(undefined, fn(null));
       assert.equal(undefined, fn(undefined));
     });
 
     it('should return the property', function () {
+      var fn = sash.prop('a');
       assert.equal(1, fn(object));
+    });
+
+    it('should propagate undefined', function () {
+      var fn = sash.prop('fail', 'again');
+      assert.equal(undefined, fn(object));
+    });
+
+    it('should follow multiple arguments', function () {
+      var query = sash.prop('b', 'c', 0);
+      assert.equal(2, query(object));
     });
   });
 
