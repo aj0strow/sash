d2ceb4999f924aee0325dc62c257f91739166cc4
AJ Ostrow <alexander.ostrow@gmail.com>
2014-03-03 22:55:25 -0500

    sash.identity

--- sash.js
+++ sash.js
@@ -1,3 +1,7 @@
 module.exports = {
 
+  identity: function (x) {
+    return x;
+  }
+
 };
--- test/sash.test.js
+++ test/sash.test.js
@@ -1,3 +1,10 @@
 describe('Sash', function () {
 
+  describe('.identity', function () {
+    it('should return whats passed in', function () {
+      var object = {};
+      assert.equal(object, sash.identity(object));
+    });
+  });
+
 });
